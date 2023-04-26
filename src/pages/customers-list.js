3dc79255f70a31bacd937dee94cbc7436c00306f
sparkmahadi <mahadi.development@gmail.com>
2023-04-26 13:10:10 +0600

    added routes

---
+++ src/pages/customers-list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CustomersList = () => {
+    return (
+        <div>
+            <h3>this is customers list</h3>
+        </div>
+    );
+};
+
+export default CustomersList;
---
+++ src/pages/products-list.js
@@ -0,0 +1,11 @@
+
+
+const ProductsList = () => {
+    return (
+        <div>
+            <h3>products list</h3>
+        </div>
+    );
+};
+
+export default ProductsList;
